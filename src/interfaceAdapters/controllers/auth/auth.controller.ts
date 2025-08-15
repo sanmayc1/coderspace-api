35f267346b8fc2e86d82515b8ec6a96c92c85357
Sanmay C <sanmayc1@gmail.com>
2025-08-15 21:51:16 +0530

    signup feature initial commit

--- src/interfaceAdapters/controllers/auth/auth.controller.ts
+++ src/interfaceAdapters/controllers/auth/auth.controller.ts
@@ -1,8 +1,11 @@
 import { inject, injectable } from "tsyringe";
+import type { IUserResgisterUseCase } from "../../../entities/useCaseInterfaces/auth/register-usecase.interface.js";
 
 @injectable()
 class AuthController {
-    constructor(@inject("")){
-          
-    }
+  constructor(
+    @inject("IUserRegisterUsecase")
+    private registerUsecase: IUserResgisterUseCase
+  ) {}
+  
 }
