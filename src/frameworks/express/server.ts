c5fde911eb0bd3ae88636e1116f3e81c2db09baa
Sanmay <sanmayc1@gmail.com>
2026-04-18 16:00:02 +0530

    update cors

--- src/frameworks/express/server.ts
+++ src/frameworks/express/server.ts
@@ -24,7 +24,7 @@ export class Server {
     this._app = express();
     this._server = createServer(this._app);
     this._io = new SocketServer(this._server, {
-      cors: { origin: [config.client.uri ,config.client.uri2], credentials: true },
+      cors: { origin: [config.client.uri, config.client.uri2], credentials: true },
     });
     authMiddleware.socketAuthMiddleware(this._io);
     socketHandler.registerChatSocketHandlers(this._io);
@@ -34,14 +34,19 @@ export class Server {
   }
 
   private configureMiddleware(): void {
-    this._app.use(cors({ origin: [config.client.uri ,config.client.uri2], credentials: true }));
+    const corsOptions = {
+      origin: [config.client.uri, config.client.uri2],
+      credentials: true,
+    };
+
+    this._app.use(cors(corsOptions));
+    this._app.options('*', cors(corsOptions));
     this._app.use(express.json());
     this._app.use(express.urlencoded({ extended: true }));
     this._app.use(cookieParser(config.cookieSecret));
   }
 
   private configureRouter(): void {
- 
     this._app.use('/api/v1/auth', authRoutes.router);
     this._app.use('/api/v1/admin', adminRoutes.router);
     this._app.use('/api/v1/user', userRoutes.router);
